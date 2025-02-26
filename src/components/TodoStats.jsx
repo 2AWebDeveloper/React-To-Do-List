78aa3b128de51f3f603cb255fe40691bd0560e32
2AWebDeveloper <ali.webdev2002@gmail.com>
2025-02-26 12:59:52 +0330

    change space between title and value

--- src/components/TodoStats.jsx
+++ src/components/TodoStats.jsx
@@ -4,13 +4,13 @@ export default function TodoStats({ todos }) {
 
   return (
     <div className="flex items-center justify-between mt-10 sm:mt-16">
-      <div className="flex items-start gap-2.5">
+      <div className="flex items-start gap-2">
         <span className="font-bold text-sm text-blue">Tasks created</span>
         <span className="px-2 py-0.5 rounded-full text-xs font-bold bg-gray-400">
           {allTodos}
         </span>
       </div>
-      <div className="flex items-start gap-2.5">
+      <div className="flex items-start gap-2">
         <span className="font-bold text-sm text-purple">Completed</span>
         <span className="px-2 py-0.5 rounded-full text-xs font-bold bg-gray-400">
           {completedTodos} of {allTodos}
