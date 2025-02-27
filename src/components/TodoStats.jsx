66ac41be6bf3135eb205d64ca249d7132b1394b1
2AWebDeveloper <ali.webdev2002@gmail.com>
2025-02-27 14:16:39 +0330

    update stats value style

--- src/components/TodoStats.jsx
+++ src/components/TodoStats.jsx
@@ -4,15 +4,15 @@ export default function TodoStats({ todos }) {
 
   return (
     <div className="flex items-center justify-between mt-10 sm:mt-16">
-      <div className="flex items-start gap-2">
+      <div className="flex items-center gap-2">
         <span className="font-bold text-sm text-blue">Tasks created</span>
-        <span className="px-2 py-0.5 rounded-full text-xs font-bold bg-gray-400">
+        <span className="px-2 pt-0.5 pb-px rounded-full text-xs font-bold bg-gray-400">
           {allTodos}
         </span>
       </div>
-      <div className="flex items-start gap-2">
+      <div className="flex items-center gap-2">
         <span className="font-bold text-sm text-purple">Completed</span>
-        <span className="px-2 py-0.5 rounded-full text-xs font-bold bg-gray-400">
+        <span className="px-2 pt-0.5 pb-px rounded-full text-xs font-bold bg-gray-400">
           {completedTodos} of {allTodos}
         </span>
       </div>
