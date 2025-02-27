b8afdf85efc6b1c6a6e3c620ddeb020784bb3648
2AWebDeveloper <ali.webdev2002@gmail.com>
2025-02-27 15:54:06 +0330

    update form input validation and change style

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,11 +3,12 @@ import IconPlus from "../svg/IconPlus";
 
 export default function TodoForm({ onAddNewTodo }) {
   const [content, setContent] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
 
   function handleFormSubmit(e) {
     e.preventDefault();
-    if (!content) {
-      alert("Please complete the field.");
+    if (!content.trim()) {
+      setIsInvalid(true);
       return;
     }
     const newTodo = {
@@ -19,6 +20,13 @@ export default function TodoForm({ onAddNewTodo }) {
     setContent("");
   }
 
+  function handleInputChange(e) {
+    setContent(e.target.value);
+    if (isInvalid && e.target.value !== "") {
+      setIsInvalid(false);
+    }
+  }
+
   return (
     <form
       className="flex items-center gap-2"
@@ -27,9 +35,11 @@ export default function TodoForm({ onAddNewTodo }) {
       <input
         type="text"
         placeholder="Add a new task ..."
-        className="grow w-full h-13 px-4 outline-none rounded-lg bg-gray-500 shadow-sm shadow-gray-700  transition-all border border-gray-500 focus:border-dark-purple placeholder:text-gray-300"
+        className={`${
+          isInvalid ? "invalid" : ""
+        } grow w-full h-13 px-4 outline-none rounded-lg bg-gray-500 shadow-sm shadow-gray-700  transition-all border border-gray-500 focus:border-dark-purple placeholder:text-gray-300`}
         value={content}
-        onChange={(e) => setContent(e.target.value)}
+        onChange={handleInputChange}
       />
       <button className="flex items-center justify-center h-13 aspect-square rounded-lg bg-dark-blue transition-all delay-75 hover:bg-blue">
         <IconPlus className={"size-6"} />
